1fd391351782711bdf1cc23e6538046074526b86
Michael Ortali <github@xethorn.net>
2015-06-01 17:17:58 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='Garcon',
-    version='0.0.4',
+    version='0.0.5',
     url='https://github.com/xethorn/garcon/',
     author='Michael Ortali',
     author_email='hello@xethorn.net',
