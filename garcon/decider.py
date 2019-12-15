45049a7b65c0dbbf5fe7387e9e8c6366bbce26f7
Vikash Patel <vpatel@theorchard.com>
2019-12-15 21:10:03 +0000

    test the state getter

--- garcon/decider.py
+++ garcon/decider.py
@@ -39,7 +39,6 @@ class DeciderWorker(swf.Decider, log.GarconLogger):
         if register:
             self.register()
 
-    @print_history
     def get_history(self, poll):
         """Get all the history.
 
@@ -63,6 +62,7 @@ class DeciderWorker(swf.Decider, log.GarconLogger):
         # Remove all the events that are related to decisions and only.
         return [e for e in events if not e['eventType'].startswith('Decision')]
 
+    @print_history
     def get_activity_states(self, history):
         """Get the activity states from the history.
 
