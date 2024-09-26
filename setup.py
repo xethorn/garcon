a21c89eae175e89c6deade06c420e5ca5dc357fa
Michael Ortali <michael.ortali@gmail.com>
2024-09-26 05:09:52 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='Garcon',
-    version='1.1.0',
+    version='1.1.1',
     url='https://github.com/xethorn/garcon/',
     author='Michael Ortali',
     author_email='hello@xethorn.net',
